1808465009b2452dcbc1360ad92ef1dda2d3025a
Nicolas Schweitzer <n.schweitzer10@gmail.com>
2018-05-14 17:35:43 +0200

    with 2 players

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,14 +3,19 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
+    session[:player_1] = params[:player_1] unless session[:player_1]
+    session[:player_2] = params[:player_2] unless session[:player_2]
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
     session[:score] = 0 unless session[:score]
+    session[:score_2] = 0 unless session[:score_2]
   end
 
   def score
     @result = 0
+    @result_2 = 0
     @letters = params[:letters].split(' ')
     @word = params[:word]
+    @word_2 = params[:word_2]
     if included?(@word, @letters)
       if english_word?(@word)
         @message = "Congratulations, #{@word.upcase} is a valid English word!"
@@ -21,7 +26,21 @@ class GamesController < ApplicationController
     else
       @message = "#{@word} is not a valid word for this grid"
     end
+    if included?(@word_2, @letters)
+      if english_word?(@word_2)
+        @message_2 = "Congratulations, #{@word_2.upcase} is a valid English word!"
+        @result_2 += @word_2.length**2
+        else
+        @message_2 = "Sorry but #{@word_2} does not seen to be a valid English word."
+      end
+    else
+      @message_2 = "#{@word_2} is not a valid word for this grid"
+    end
     session[:score] += @result
+    session[:score_2] += @result_2
+  end
+
+  def start
   end
 
   def english_word?(word)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'new', to: 'games#new', as: :new
   get 'score', to: 'games#score', as: :score
   post 'score', to: 'games#score'
+  get 'start', to: 'games#start'
+  post 'new', to: 'games#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
